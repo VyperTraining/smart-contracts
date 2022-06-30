20f51b55eb86460329787ca681a9977f9da0ae01
Diogo Perillo <diogo.perillo@gmail.com>
2022-06-30 11:12:32 -0600

    Refactor some tests

--- tests/test_HelloWorld.py
+++ tests/test_HelloWorld.py
@@ -1,27 +1,37 @@
+import pytest
+from ape.utils import misc
+
+
+@pytest.fixture
+def greet(hello_world_contract):
+    def greet_function(sender, message):
+        price = hello_world_contract.price()
+        hello_world_contract.setGreeting(message, sender=sender, value=price)
+        assert hello_world_contract.greet() == "Hello " + message
+        assert hello_world_contract.lastChanger() == sender
+
+    return greet_function
+
+
 def test_default_greeting(hello_world_contract):
     assert hello_world_contract.greet() == "Hello World!"
 
 
-def test_set_greeting(hello_world_contract, owner, bob):
+def test_set_greeting(hello_world_contract, owner, bob, greet):
     assert hello_world_contract.greet() == "Hello World!"
-    assert (
-        hello_world_contract.lastChanger()
-        == "0x0000000000000000000000000000000000000000"
-    )
+    assert hello_world_contract.lastChanger() == misc.ZERO_ADDRESS
 
     price = hello_world_contract.price()
 
-    hello_world_contract.setGreeting("Mars!", sender=owner, value=price)
-    assert hello_world_contract.greet() == "Hello Mars!"
-    assert hello_world_contract.lastChanger() == owner
-
-    hello_world_contract.setGreeting("Earth!", sender=bob, value=price * 2)
-    assert hello_world_contract.greet() == "Hello Earth!"
-    assert hello_world_contract.lastChanger() == bob
+    greet(owner, "Mars!")
+    greet(bob, "Earth!")
 
     assert hello_world_contract.balance == price * 3
 
-    # Withdraw
+
+def test_withdraw(hello_world_contract, owner, bob, greet):
+    greet(bob, "Mars!")
+
     ownerBalance = owner.balance
     hello_world_contract.withdraw(sender=bob)
     assert owner.balance > ownerBalance
